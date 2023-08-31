30cac93f2d2efc804a8b57e87b43092bd816e74c
BlockGem <kifeanyi216@gmail.com>
2023-08-31 03:16:17 +0100

    Chaining Multiple Middleware function

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,6 +4,17 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkID = (req, res, next, val) => {
+  console.log(`Tour id is: ${val}`);
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID provided',
+    });
+  }
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
@@ -23,12 +34,6 @@ exports.getTour = (req, res) => {
   const tour = tours.find((el) => el.id === id);
 
   //   if (id > tours.length) {
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID provided',
-    });
-  }
 
   res.status(200).json({
     status: 'success',
@@ -63,13 +68,6 @@ exports.createTour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID provided',
-    });
-  }
-
   res.status(204).json({
     status: 'success',
     data: null,
@@ -77,13 +75,6 @@ exports.deleteTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID provided',
-    });
-  }
-
   res.status(200).json({
     status: 'success',
     data: {
