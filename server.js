29d1f488f8b8146bc17169e430942d8679d62455
BlockGem <kifeanyi216@gmail.com>
2023-09-08 12:18:05 +0100

    Refactoring for MVC

--- server.js
+++ server.js
@@ -15,39 +15,7 @@ mongoose
     useCreateIndex: true,
     useFindAndModify: false,
   })
-  .then((con) => console.log('DB connection successfully established!'));
-
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: ['true', 'A tour must have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: ['true', 'A tour must have a price'],
-  },
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-  name: 'The Park Camper',
-  price: 997,
-});
-
-testTour
-  .save()
-  .then((doc) => {
-    console.log(doc);
-  })
-  .catch((err) => {
-    console.log('ERROR 💥:', err);
-  });
+  .then(() => console.log('DB connection successfully established!'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
