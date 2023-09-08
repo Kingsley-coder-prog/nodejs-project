258da41d80405c2d7a7bffabba4dad595dc35859
BlockGem <kifeanyi216@gmail.com>
2023-09-08 10:42:16 +0100

    Creating Simple Tour Model

--- server.js
+++ server.js
@@ -17,8 +17,23 @@ mongoose
   })
   .then((con) => console.log('DB connection successfully established!'));
 
-// console.log(app.get('env'));
-// console.log(process.env);
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: ['true', 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: ['true', 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
