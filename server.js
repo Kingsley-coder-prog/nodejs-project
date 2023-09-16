bb4cf8df0923c1f50fc82f45053d331d2d666c3d
BlockGem <kifeanyi216@gmail.com>
2023-09-16 03:43:27 +0100

    Errors Outside Express: Unhandled Rejections

--- server.js
+++ server.js
@@ -22,3 +22,11 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
+});
