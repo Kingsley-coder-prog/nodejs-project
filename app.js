3c63f6dc4f9ec1aff4862ca91833a3716bc4bb4a
BlockGem <kifeanyi216@gmail.com>
2023-09-26 10:12:45 +0100

    Protecting Tour Routes 1

--- app.js
+++ app.js
@@ -19,15 +19,9 @@ app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-
+  // console.log(req.headers);
   next();
 });
-
-// app.get('/api/v1/tours', getAllTours);
-// app.post('/api/v1/tours', createTour);
-// app.get('/api/v1/tours/:id', getTour);
-// app.patch('/api/v1/tours/:id', updateTour);
-// app.delete('/api/v1/tours/:id', deleteTour);
 
 // 3) ROUTES
 
