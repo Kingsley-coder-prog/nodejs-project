aac3bb221fe0a04393f27f99f575dedd78d119c5
BlockGem <kifeanyi216@gmail.com>
2023-10-04 22:51:53 +0100

    Handling JWTExpired Error

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -21,8 +21,11 @@ const handleValidationErrorDB = (err) => {
   return new AppError(message, 400);
 };
 
-const handleJWTError = (err) =>
+const handleJWTError = () =>
   new AppError('Invalid token. Please login again!', 401);
+
+const handleJWTExpiredError = () =>
+  new AppError('Your token has expired! Please login again.', 401);
 
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
@@ -71,7 +74,8 @@ module.exports = (err, req, res, next) => {
     if (error.name === 'ValidationError')
       error = handleValidationErrorDB(error);
 
-    if (error.name === 'JsonWebTokenError') error = handleJWTError(error);
+    if (error.name === 'JsonWebTokenError') error = handleJWTError();
+    if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
 
     sendErrorProd(error, res);
   }
