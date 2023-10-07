55948dd5fc09f1287bcce39abb5030196ffef627
BlockGem <kifeanyi216@gmail.com>
2023-10-07 21:26:01 +0100

    AppError Refactoring

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -86,7 +86,7 @@ exports.deleteTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findByIdAndDelete(req.params.id);
 
   if (!tour) {
-    return next(new AppError('No tour found with that ID', 404));
+    return next(new AppError('No tour was found with that ID', 404));
   }
   res.status(204).json({
     status: 'success',
