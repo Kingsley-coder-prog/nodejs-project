a947c2dea7dc42e5e00442092df189bff0df58e4
BlockGem <kifeanyi216@gmail.com>
2023-10-13 17:36:04 +0100

    Server Code Reset

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ mongoose
   .then(() => console.log('DB connection successfully established!'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
 
