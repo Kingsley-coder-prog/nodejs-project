edcd77f70992b5aab3e97a7e009dcc9bae6e1ac5
BlockGem <kifeanyi216@gmail.com>
2023-10-17 13:20:51 +0100

    Authorization: User Role and Permission

--- models/userModel.js
+++ models/userModel.js
@@ -7,7 +7,6 @@ const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'Please tell us your name'],
-    // unique: true,
   },
   email: {
     type: String,
@@ -17,6 +16,11 @@ const userSchema = new mongoose.Schema({
     validate: [validator.isEmail, 'Please provide a valid email'],
   },
   photo: String,
+  role: {
+    type: String,
+    enum: ['user', 'guide', 'lead-guide', 'admin'],
+    default: 'user',
+  },
   password: {
     type: String,
     required: [true, 'Please provide a password'],
