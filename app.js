2720d628c54158eaf90411780318157308e8880a
BlockGem <kifeanyi216@gmail.com>
2023-10-20 20:49:14 +0100

    Sending Emails With Nodemailer 2

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const morgan = require('morgan');
 
 const AppError = require('./utils/appError');
@@ -15,6 +16,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.use(express.json());
+
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
--- utils/email.js
+++ utils/email.js
@@ -5,6 +5,7 @@ const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
+
     auth: {
       user: process.env.EMAIL_USERNAME,
       pass: process.env.EMAIL_PASSWORD,
@@ -21,6 +22,40 @@ const sendEmail = async (options) => {
 
   // 3) Actually send the email
   await transporter.sendMail(mailOptions);
+  console.log(process.env.EMAIL_HOST, 'hi');
 };
 
 module.exports = sendEmail;
+
+// const nodemailer = require('nodemailer');
+
+// const sendEmail = async (options) => {
+//   // First create a transporter
+//   const transporter = nodemailer.createTransport({
+//     host: `${process.env.EMAIL_HOST}`,
+//     port: process.env.EMAIL_PORT,
+//     secure: false,
+//     logger: true,
+//     auth: {
+//       user: `${process.env.EMAIL_USERNAME}`,
+//       pass: `${process.env.EMAIL_PASSWORD}`,
+//     },
+//     tls: {
+//       rejectUnauthorized: false,
+//     },
+//   });
+
+//   // Define the email options
+//   const mailOptions = {
+//     from: 'Swayam Goswami <swayamgoswami989@gmail.com',
+//     to: options.email,
+//     subject: options.subject,
+//     text: options.message,
+//   };
+
+//   // Send the email
+//   await transporter.sendMail(mailOptions);
+//   console.log(process.env.EMAIL_HOST, 'hi');
+// };
+
+// module.exports = sendEmail;
