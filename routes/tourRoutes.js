07e9ffa4aeba10b4410cec6f0f018cb3e274f9df
BlockGem <kifeanyi216@gmail.com>
2023-10-31 10:08:48 +0100

    Nested Routes With Express

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
 const authController = require('./../controllers/authController');
-const reviewController = require('./../controllers/reviewController');
+const reviewRouter = require('./../routes/reviewRoutes');
 
 const router = express.Router();
 
 // router.param('id', tourController.checkID);
+
+// POST /tour/2345ad/reviews
+// GET /tour/2345ad/reviews
+
+router.use('/:tourId/reviews', reviewRouter);
+
 router
   .route('/top-5-cheap')
   .get(tourController.aliasTopTours, tourController.getAllTours);
@@ -25,17 +31,6 @@ router
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
     tourController.deleteTour,
-  );
-
-// POST /tour/2345ad/reviews
-// GET /tour/2345ad/reviews
-// GET /tour/2345ad/reviews/5948fda
-router
-  .route('/:tourId/reviews')
-  .post(
-    authController.protect,
-    authController.restrictTo('user'),
-    reviewController.createReview,
   );
 
 module.exports = router;
