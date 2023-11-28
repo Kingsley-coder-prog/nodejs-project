02c873c959b05f2878d57dac036b4ca6d9439130
BlockGem <kifeanyi216@gmail.com>
2023-11-28 23:32:33 +0100

    Login Errors

--- public/js/login.js
+++ public/js/login.js
@@ -2,29 +2,29 @@ import axios from 'https://cdn.jsdelivr.net/npm/axios@1.6.2/+esm';
 // https://cdn.jsdelivr.net/npm/axios@1.6.2/+esm
 
 const login = async (email, password) => {
-  location.assign('/');
-  console.log(email, password);
-  // try {
-  //   const res = await axios({
-  //     method: 'POST',
-  //     url: 'http://127.0.0.1:8000/api/v1/users/login',
-  //     data: {
-  //       email,
-  //       password,
-  //     },
-  //   });
-  //   console.log(res);
+  // location.assign('/');
+  // console.log(email, password);
+  try {
+    const res = await axios({
+      method: 'POST',
+      url: 'http://127.0.0.1:8000/api/v1/users/login',
+      data: {
+        email,
+        password,
+      },
+    });
+    console.log(res);
 
-  //   if (res.data.status === 'success') {
-  //     alert('Logged in successfully');
-  //     window.setTimeout(() => {
-  //       location.assign('/');
-  //     }, 1500);
-  //   }
-  // } catch (err) {
-  //   console.log(err.errresponse.data);
-  //   alert(err.response.data.message);
-  // }
+    if (res.data.status === 'success') {
+      alert('Logged in successfully');
+      window.setTimeout(() => {
+        location.assign('/');
+      }, 1500);
+    }
+  } catch (err) {
+    console.log(err.errresponse.data);
+    alert(err.response.data.message);
+  }
 };
 
 document.querySelector('.form').addEventListener('submit', (e) => {
